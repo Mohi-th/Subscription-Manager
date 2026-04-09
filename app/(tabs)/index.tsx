dd61beb11608cf67e718c74692acab99436478d9
Mohi-th <mohithmohith880@gmail.com>
2026-04-09 11:20:29 +0530

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,11 @@ import SaveScreen from "../components/SafeAreaView";
 
 export default function App() {
   return (
-    <SaveScreen >
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href={"/OnBoarding"}>onboarding</Link>
-      <Link href={"/(auth)/SignIn"} className="border px-5 py-2">sign in</Link>
-      <Link href={"/(auth)/SignUp"} className="border px-5 py-2">create new account</Link>
-      {/* <Link href="/Subscriptions/spotify" className="border px-5 py-2">Spotify Subscription</Link>
-      <Link href={{
-        pathname:"/Subscriptions/[id]",
-        params:{id:"claude"}
-      }} className="border px-5 py-2">Claude Max subscription</Link> */}
+    <SaveScreen>
+      <Text className="text-7xl font-sans-extrabold text-success">Home</Text>
+      <Link href={"/OnBoarding"} className="mt-4 font-sans-bold rounded bg-primary text-white p-4">onboarding</Link>
+      <Link href={"/(auth)/SignIn"} className="mt-4 font-sans-bold rounded bg-primary text-white p-4">sign in</Link>
+      <Link href={"/(auth)/SignUp"} className="mt-4 font-sans-bold rounded bg-primary text-white p-4">create new account</Link>
     </SaveScreen>
   );
 }
