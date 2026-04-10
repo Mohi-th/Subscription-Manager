7d7b30a060f7687e637f732975a5c00e58655b0e
Mohi-th <mohithmohith880@gmail.com>
2026-04-10 11:41:06 +0530

    feat : implemented authentication using clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,25 @@ import { SplashScreen, Stack } from "expo-router";
 import "@/global.css"
 import { useFonts } from "expo-font"
 import { useEffect } from "react";
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { useAuth } from "@clerk/expo";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+function RootLayoutContant() {
+
+  const { isSignedIn } = useAuth()
+
+<<<<<<< HEAD
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+=======
+>>>>>>> 4b79358 (feat : implemented authentication using clerk)
   const [fontsLoaded] = useFonts({
     'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
     'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
@@ -15,13 +30,31 @@ export default function RootLayout() {
     'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
   })
 
-  useEffect(()=>{
-    if(fontsLoaded){
+  useEffect(() => {
+    if (fontsLoaded) {
       SplashScreen.hideAsync()
     }
-  },[fontsLoaded])
+  }, [fontsLoaded])
 
-  if(!fontsLoaded)return null
-  
-  return <Stack screenOptions={{ headerShown: false }} initialRouteName="(tabs)" />
+  if (!fontsLoaded) return null
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(auth)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!!isSignedIn}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+    </Stack>
+  )
 }
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <RootLayoutContant />
+    </ClerkProvider>
+  )
+}
+
